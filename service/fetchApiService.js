7439a7d97e1fc74acf0f5e4698c7d2a4d3d5de5b
Heitor Santos <heitorg33@gmail.com>
2022-03-15 10:40:16 -0300

    Feat: constructor error

--- service/fetchApiService.js
+++ service/fetchApiService.js
@@ -3,7 +3,7 @@ const instance = require('../utils/request');
 const fetchApiService = async() => {
   const response = await instance.get('takenet/repos')
   const date = response.data;
-  if(!date) return('Api não encontrada');
+  if(!date) throw (404, 'Not found');
   const searchData = await date.sort(function(a,b) {
     const date1 = new Date(a.created_at);
     const date2 = new Date(b.created_at);
